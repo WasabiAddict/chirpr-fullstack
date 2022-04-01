e542921c8130363dbbbe63d1e749785037bc20b2
WasabiAddict <davidtltran@gmail.com>
2022-04-01 15:15:20 -0500

    more

--- client/App.jsx
+++ client/App.jsx
@@ -6,8 +6,26 @@ import ChirpCard from "./components/ChirpCard.jsx";
 const App = () => {
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
-  const [chirps, setChirps] = useState();
-
+  const [chirps, setChirps] = useState([
+    {
+      id: uuidv4(),
+      username: "Josh",
+      message: "This is the chirp body!",
+      created: moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
+    },
+    {
+      id: uuidv4(),
+      username: "Haylee",
+      message: "Hello!",
+      created: moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
+    },
+    {
+      id: uuidv4(),
+      username: "Garrett",
+      message: "I'm not mad!",
+      created: moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
+    },
+  ]);
 
   const handleUsernameChange = (e) => setUsername(e.target.value);
   const handleMessageChange = (e) => setMessage(e.target.value);
@@ -22,6 +40,54 @@ const App = () => {
     };
 
     setChirps([...chirps, newChirp]);
+  };
+
+  //rest
+  const getChirps = () => {
+    fetch('http://localhost:3000/api/chirps')
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(allChirps => {
+        console.log(allChirps[0]);
+        setChirps(allChirps);
+      });
+  }
+
+  useEffect(() => {
+    getChirps();
+  }, []);
+
+  //create
+  const postChirp = async () => {
+    console.log('test');
+    const userData = {
+      userid: 6,
+      content: message,
+      location: 'Secret Test Location'
+    };
+    
+    try {
+      const add = await fetch('http://localhost:3000/api/chirps/', {
+        method: "POST",
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(userData)
+      })
+    } catch {
+      console.log(e);
+    }
+
+  };
+
+  //delete
+  const handleDeleteChirp = (id) => {
+    fetch('http://localhost:3000/api/chirps/' + id, {
+      method: "DELETE"
+    })
+    getChirps();
   };
 
   return (
